e73e7d10f1fcddd262645e831d3508c3b4735a75
cynthianduku <cynthiandukuu@gmail.com>
2025-08-18 14:04:25 +0300

    Fix JSX syntax error

--- form-handling-react/vite.config.js
+++ form-handling-react/vite.config.js
@@ -1,6 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: 'react',
+      babel: {
+        presets: [
+          [
+            '@babel/preset-react',
+            {
+              runtime: 'automatic',
+              // ⬇️ Add this to support JSX in .js files
+              development: true
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
